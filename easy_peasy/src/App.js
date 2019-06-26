d2c99ebb0e05f81767c410aa7744040742b58a88
Josh Silverman <jrsilverman71@hotmail.com>
2019-06-26 12:17:22 -0700

    setting up app

---
+++ easy_peasy/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Todos />
+      <AddTodo />
+    </div>
+  );
+}
+
+export default App;
